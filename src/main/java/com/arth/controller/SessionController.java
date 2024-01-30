8b675c3940714380bb348c619f6219b5e196da68
Tejas Shah <tejasshah2k19@gmail.com>
2024-01-30 12:29:00 +0530

    signup and read data

--- src/main/java/com/arth/controller/SessionController.java
+++ src/main/java/com/arth/controller/SessionController.java
@@ -2,18 +2,39 @@ package com.arth.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.arth.bean.UserBean;
 
 @Controller
 public class SessionController {
 
-	@GetMapping("/welcome")
-	public String welcome(){
-		return "Welcome";//jsp name 
+	@GetMapping("/")
+	public String welcome() {
+		return "Welcome";// jsp name
 	}
 
 	@GetMapping("/signup")
 	public String signup() {
 		return "Signup";
 	}
-	 
+
+	@GetMapping("/login")
+	public String login() {
+		return "Login";
+	}
+
+	@PostMapping("/saveuser")
+	public String saveUser(UserBean user) {
+		// read
+		System.out.println(user.getFirstName());
+		System.out.println(user.getLastName());
+		System.out.println(user.getEmail());
+		System.out.println(user.getPassword());
+		// validation
+		// database save
+
+		System.out.println("SaveUser");
+		return "Home";
+	}
 }
--- src/main/webapp/WEB-INF/views/Signup.jsp
+++ src/main/webapp/WEB-INF/views/Signup.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,9 +7,18 @@
 <title>Signup</title>
 </head>
 <body>
-<h2>Signup</h2>
+	<h2>Signup</h2>
 
 
+	<form action="saveuser"  method="post" >
+		FirstName : <input type="text" name="firstName"  placeholder="Enter FirstName"/><Br><br> 
+		LastName : <input type="text" name="lastName"/><Br><br>
+		Email :<input type="text" name="email"/><Br><br>
+		Password : <input type="password" name="password"/><Br><br>
+		
+		<input type="submit"  value="SignUP"/>
+	</form>
 
+	<a href="login">Login</a>
 </body>
 </html>

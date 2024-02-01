333bceddbdad3cffd6c6c91fb43611ca117d0b98
Tejas Shah <tejasshah2k19@gmail.com>
2024-02-01 12:31:10 +0530

    Role Entity

---
+++ src/main/java/com/arth/entity/RoleEntity.java
@@ -0,0 +1,36 @@
+package com.arth.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class RoleEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer roleId;
+	private String roleName;
+ 
+	public Integer getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(Integer roleId) {
+		this.roleId = roleId;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	 
+
+}
